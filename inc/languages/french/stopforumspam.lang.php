1412b29effc5356d2ddd07b5dcba264f5bb5e0e9
tdubourg <plop@plop.fake>
2013-02-20 22:24:13 +0100

    Added french language

---
+++ inc/languages/french/stopforumspam.lang.php
@@ -0,0 +1,14 @@
+<?php
+// ACP
+$l['admin_check_username'] = "Vérifier le noms d'utilisateur ?";
+$l['admin_check_username_desc'] = "Vérifier le nom d'utilisateur dans la base de données de StopForumSpam ?";
+$l['admin_check_email'] = "Vérifier adresse email ?";
+$l['admin_check_email_desc'] = "Vérifier l'adresse email dans la base de données de StopForumSpam ?";
+$l['admin_check_ip'] = "Vérifier l'adresse IP ?";
+$l['admin_check_ip_desc'] = "Vérifier l'adresse IP dans la base de données de StopForumSpam ?";
+
+// Front end
+$l['spam_contact_error'] = "StopForumSpam.com n'a pas pu être contacté.";
+$l['spam_error'] = "Une erreur s'est produite lors de la validation de votre insccription. Merci de contacter l'administrateur ou de réessayer plus tard.";
+$l['spam_blocked'] = "Votre adresse email ou votre IP correspond à celle de spammeurs connus et vous ne pouvez par conséquent pas vous inscrire ici. Si vous pensez qu'il s'agit d'une erreur, merci de contacter l'administrateur ou de réessayer plus tard.";
+?>
